114b36d2840ab5a7390b19e73ec15291c261c48a
yhattav <yhattav@gmail.com>
2016-05-21 15:46:34 +0300

    chaniging the dependancies

---
+++ api/controllers/FriendsController.js
@@ -0,0 +1,11 @@
+/**
+ * FriendsController
+ *
+ * @description :: Server-side logic for managing Friends
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
